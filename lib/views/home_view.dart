98c3a0f18d6ef57a8bcb78a7ace1a60fcec48238
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-05-31 01:05:52 +0300

    create card header item

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 
+import 'package:easy_pos_r5/widgets/card_header_item.dart';
 import 'package:easy_pos_r5/widgets/row_header.dart';
 import 'package:flutter/material.dart';
 
@@ -21,14 +22,23 @@ class _HomeViewState extends State<HomeView> {
             children: [
               Expanded(
                 child: Container(
-                  height: MediaQuery.of(context).size.height/3,
+                  height: MediaQuery.of(context).size.height/3 +18,
                   color: Theme.of(context).primaryColor,
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                  child:  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         RowHeader(),
+                        const SizedBox(height: 12,),
+                        CardHeaderItem(
+                          title: "Exchange Rate",
+                          subTitle: "1 USD = 50 Egp",
+                        ),
+                        CardHeaderItem(
+                          title: "Today's Sales",
+                          subTitle: "1000 EGP",
+                        ),
                       ],
                     ),
                   ),
@@ -41,5 +51,7 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 }
+
+
 
 
---
+++ lib/widgets/card_header_item.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+
+class CardHeaderItem extends StatelessWidget {
+  final String subTitle;
+  final String title;
+  const CardHeaderItem({
+    required this.subTitle,
+    required this.title,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Card(
+        elevation: 0,
+        color: const Color(0xff206ce1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title,
+                style: const TextStyle(
+                    color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                title,
+                style: const  TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
