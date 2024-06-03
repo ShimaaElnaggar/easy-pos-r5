240a7b459534abb530a81e9092d4f746bd6e20a6
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-06-03 20:15:44 +0300

    create customTextFormField

---
+++ lib/widgets/custom_text_form_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final TextInputAction textInputAction;
+  final TextInputType keyboardType;
+  final String label;
+  final TextEditingController controller;
+  final IconButton? suffixIcon;
+
+  const CustomTextFormField(
+      {this.suffixIcon,
+      required this.textInputAction,
+      required this.keyboardType,
+      required this.label,
+      required this.controller,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      autocorrect: true,
+      decoration: InputDecoration(
+        suffixIcon: suffixIcon,
+        labelText: label,
+        focusedBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: Theme.of(context).primaryColor, width: 2),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 2),
+        ),
+      ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return " Please enter the valid $label ";
+        }
+        return null;
+      },
+    );
+  }
+}
